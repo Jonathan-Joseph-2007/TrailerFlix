2264ae3b13f68d710f694159d1a59977fbcadb18
jonathanjoseph-altf4 <jonathanjoseph25@karunya.edu.in>
2026-02-11 16:53:57 +0530

    final deploy fix

--- app.py
+++ app.py
@@ -86,5 +86,8 @@ def get_movie():
         "youtube_id": video_id
     })
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
